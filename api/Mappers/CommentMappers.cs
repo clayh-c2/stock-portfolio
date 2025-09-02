ea51864ba988e58f3d516fd7afc40bd08f3b54af
Clay Huang <clayhzc2@gmail.com>
2025-09-02 16:47:29 -0400

    implemented create comment funcionality, including dto, repository, mapper, and relevant stock repository logic

--- api/Mappers/CommentMappers.cs
+++ api/Mappers/CommentMappers.cs
@@ -20,5 +20,15 @@ namespace api.Mappers
                 StockId = commentModel.StockId
             };
         }
+
+        public static Comment ToCommentFromCreate(this CreateCommentRequestDto commentDto, int stockId)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                StockId = stockId
+            };
+        }
     }
 }
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -67,5 +67,10 @@ namespace api.Repository
             await _context.SaveChangesAsync();
             return stockModel;
         }
+
+        public Task<bool> StockExists(int id)
+        {
+            return _context.Stocks.AnyAsync(s => s.Id == id);
+        }
     }
 }
