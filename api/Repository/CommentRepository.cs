e7659a6d4dad7f862d793dad26814745fca8c73f
Clay Huang <clayhzc2@gmail.com>
2025-09-03 02:34:27 -0400

    implemented comment delete functionality including controller and repository

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -49,5 +49,19 @@ namespace api.Repository
             await _context.SaveChangesAsync();
             return existingComment;
         }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+            if (commentModel == null)
+            {
+                return null;
+            }
+
+            _context.Comments.Remove(commentModel);
+            await _context.SaveChangesAsync();
+
+            return commentModel;
+        }
     }
 }
